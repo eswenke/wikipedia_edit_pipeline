cf2866ebdf66eeef5fb8cda9e22fbe11ea93a216
Ethan Swenke <swenke.ethan.us@gmail.com>
2026-01-23 14:39:07 -0800

    proper processing and json conversion

--- change_proc.py
+++ change_proc.py
@@ -4,4 +4,37 @@ import json
 
 
 async def wiki_connect():
+    # establish uri and headers in accordance to wikimedia robot policy: https://wikitech.wikimedia.org/wiki/Robot_policy
     uri = "https://stream.wikimedia.org/v2/stream/recentchange"
+    headers = {
+        "User-Agent": "WikipediaEditPipeline/1.0 (https://github.com/eswenke; swenke.ethan.us@gmail.com) aiohttp/3.13.3",
+        "Accept-Encoding": "gzip",
+    }
+
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(uri) as resp:
+            # show status of request
+            print(resp.status)
+
+            # process lines, stripping event metadata and converting useful data to json
+            i = 0
+            async for line in resp.content:
+                if line:
+                    # decode from bytes and strip whitespace
+                    clean_line = line.decode().strip()
+
+                    # find useful data about the change and convert to json
+                    if clean_line.startswith("data: "):
+                        i += 1
+                        json_data = json.loads(clean_line[6:])
+                        print(json_data["id"])
+                        print(json_data["type"])
+                        print(json_data["title"])
+                        print()
+
+                        if i > 10:
+                            exit(0)
+
+
+if __name__ == "__main__":
+    asyncio.run(wiki_connect())
