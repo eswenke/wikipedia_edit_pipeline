f286a08f41e91ebabe526223fae400531271416d
Ethan Swenke <swenke.ethan.us@gmail.com>
2026-01-28 21:38:44 -0800

    comment on future developments

--- src/connect_redis.py
+++ src/connect_redis.py
@@ -4,6 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# create RedisManager class that can be called from change_proc
+# change_proc is the primary processing file, so should we make that
+# file into a class as well, powering the engine of the pipeline?
+# then we add a postgresql connection class? seems to be the most
+# production-like and logical. then we create a visualization class
+# of sorts?
+
 r = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=int(os.getenv("REDIS_PORT")),
